135a778b294d518b1f6f956e42dad4e3370fc1b4
zhangteng <910547570@qq.com>
2020-02-25 17:09:27 +0800

    first init administrator back

---
+++ administrator-back/src/main/java/com/zhangteng/administratorback/AdministratorBackApplication.java
@@ -0,0 +1,13 @@
+package com.zhangteng.administratorback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministratorBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministratorBackApplication.class, args);
+    }
+
+}
---
+++ administrator-back/src/test/java/com/zhangteng/administratorback/AdministratorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangteng.administratorback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
