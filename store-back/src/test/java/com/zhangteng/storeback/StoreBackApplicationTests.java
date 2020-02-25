35f8cd1865c6eb50e793eae35bee7112c92298fd
zhangteng <910547570@qq.com>
2020-02-25 17:12:31 +0800

    first init store back

---
+++ store-back/src/test/java/com/zhangteng/storeback/StoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangteng.storeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
