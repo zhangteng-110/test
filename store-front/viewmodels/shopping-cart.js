623b70ed6cc90c257e028829984f00d94e578dc5
zhangteng <910547570@qq.com>
2020-03-04 20:12:12 +0800

    fix store shopping cart page total price and Realize the number of items in the store shopping cart

--- store-front/viewmodels/shopping-cart.js
+++ store-front/viewmodels/shopping-cart.js
@@ -3,6 +3,9 @@ var app = new Vue({
     data: {
         myShoppingCart: [],
     },
+    watch: {
+        
+    },
     mounted(){
         console.log("view mounted")
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
@@ -10,12 +13,15 @@ var app = new Vue({
     },
     methods: {
         handleUpdate(index,row){
+            console.log('update click');
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('更改数量成功');
         },
         handleDelete(index,row){
             console.log('delete click');
             this.myShoppingCart.splice(index, 1);
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
-
+            this.$message.success('成功删除商品');
         }
     }
 })
