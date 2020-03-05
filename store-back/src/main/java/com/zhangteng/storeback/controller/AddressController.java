85221945cd7a559dca776ab3d8656db0ed56513d
zhangteng <910547570@qq.com>
2020-03-05 23:21:54 +0800

    add store order checkout page

--- store-back/src/main/java/com/zhangteng/storeback/controller/AddressController.java
+++ store-back/src/main/java/com/zhangteng/storeback/controller/AddressController.java
@@ -4,15 +4,37 @@ package com.zhangteng.storeback.controller;
 import com.zhangteng.storeback.dto.in.AddressCreateInDTO;
 import com.zhangteng.storeback.dto.in.AddressUpdateInDTO;
 import com.zhangteng.storeback.dto.out.AddressListOutDTO;
+import com.zhangteng.storeback.po.Address;
+import com.zhangteng.storeback.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/address")
 @CrossOrigin
 public class AddressController {
 
+    @Autowired
+    private AddressService addressService;
+    @GetMapping("/getCustomerAddress")
+    public List<AddressListOutDTO> getCustomerAddress(@RequestAttribute Integer customerId){
+        List<Address> addresses = addressService.getByCustomerId(customerId);
+
+        List<AddressListOutDTO> addressListOutDTOS = addresses.stream().map(address -> {
+            AddressListOutDTO addressListOutDTO = new AddressListOutDTO();
+            addressListOutDTO.setAddressId(address.getAddressId());
+            addressListOutDTO.setTag(address.getTag());
+            addressListOutDTO.setReceiverName(address.getReceiverName());
+            addressListOutDTO.setReceiverMobile(address.getReceiverMobile());
+            addressListOutDTO.setContent(address.getContent());
+            return addressListOutDTO;
+        }).collect(Collectors.toList());
+
+        return addressListOutDTOS;
+    }
     @GetMapping("/getAddressByCustomerId")
     public List<AddressListOutDTO> getAddressByCustomerId(@RequestAttribute Integer customerId){
         return null;
