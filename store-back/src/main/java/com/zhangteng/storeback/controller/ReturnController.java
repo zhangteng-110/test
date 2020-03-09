e471cb7df4560bb41fe0a5d3acd43144d5fd9023
zhangteng <910547570@qq.com>
2020-03-09 21:05:19 +0800

    add store return get list api

--- store-back/src/main/java/com/zhangteng/storeback/controller/ReturnController.java
+++ store-back/src/main/java/com/zhangteng/storeback/controller/ReturnController.java
@@ -1,6 +1,7 @@
 package com.zhangteng.storeback.controller;
 
 
+import com.github.pagehelper.Page;
 import com.zhangteng.storeback.dto.in.ReturnApplyInDTO;
 import com.zhangteng.storeback.dto.out.PageOutDTO;
 import com.zhangteng.storeback.dto.out.ReturnListOutDTO;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/return")
@@ -47,8 +50,24 @@ public class ReturnController {
 
     @GetMapping("/getList")
     public PageOutDTO<ReturnListOutDTO> getList(@RequestAttribute Integer customerId,
-                                                @RequestParam Integer pageNum){
-        return null;
+                                                @RequestParam(required = false, defaultValue = "1") Integer pageNum){
+        Page<Return> page = returnService.getPageByCustomerId(customerId, pageNum);
+        List<ReturnListOutDTO> returnListOutDTOS = page.stream().map(aReturn -> {
+            ReturnListOutDTO returnListOutDTO = new ReturnListOutDTO();
+            returnListOutDTO.setReturnId(aReturn.getReturnId());
+            returnListOutDTO.setOrderId(aReturn.getOrderId());
+            returnListOutDTO.setCustomerId(aReturn.getCustomerId());
+            returnListOutDTO.setCustomerName(aReturn.getCustomerName());
+            returnListOutDTO.setStatus(aReturn.getStatus());
+            returnListOutDTO.setCreateTimestamp(aReturn.getCreateTime().getTime());
+            return returnListOutDTO;
+        }).collect(Collectors.toList());
+        PageOutDTO<ReturnListOutDTO> pageOutDTO = new PageOutDTO<>();
+        pageOutDTO.setTotal(page.getTotal());
+        pageOutDTO.setPageSize(page.getPageSize());
+        pageOutDTO.setPageNum(page.getPageNum());
+        pageOutDTO.setList(returnListOutDTOS);
+        return pageOutDTO;
     }
 
     @GetMapping("/getById")
