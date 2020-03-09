5450ffbeb980ca9ea232b13aba8a49be1dc81900
zhangteng <910547570@qq.com>
2020-03-09 21:23:56 +0800

    add administration return get by returnId and update api

--- administrator-back/src/main/java/com/zhangteng/administrationback/controller/ReturnController.java
+++ administrator-back/src/main/java/com/zhangteng/administrationback/controller/ReturnController.java
@@ -11,6 +11,7 @@ import com.zhangteng.administrationback.service.ReturnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -52,11 +53,34 @@ public class ReturnController {
         return pageOutDTO;
     }
     @GetMapping("/getById")
-    public ReturnShowOutDTO getById(@RequestParam Integer returnId){
-        return null;
+    public ReturnShowOutDTO getById(@RequestParam Integer returnId) {
+        Return aReturn = returnService.getById(returnId);
+        ReturnShowOutDTO returnShowOutDTO = new ReturnShowOutDTO();
+        returnShowOutDTO.setReturnId(aReturn.getReturnId());
+        returnShowOutDTO.setOrderId(aReturn.getOrderId());
+        returnShowOutDTO.setOrderTimestamp(aReturn.getOrderTime().getTime());
+        returnShowOutDTO.setCustomerId(aReturn.getCustomerId());
+        returnShowOutDTO.setCustomerName(aReturn.getCustomerName());
+        returnShowOutDTO.setMobile(aReturn.getMobile());
+        returnShowOutDTO.setEmail(aReturn.getEmail());
+        returnShowOutDTO.setStatus(aReturn.getStatus());
+        returnShowOutDTO.setAction(aReturn.getAction());
+        returnShowOutDTO.setProductCode(aReturn.getProductCode());
+        returnShowOutDTO.setProductName(aReturn.getProductName());
+        returnShowOutDTO.setQuantity(aReturn.getQuantity());
+        returnShowOutDTO.setReason(aReturn.getReason());
+        returnShowOutDTO.setOpened(aReturn.getOpened());
+        returnShowOutDTO.setComment(aReturn.getComment());
+        returnShowOutDTO.setCreateTimestamp(aReturn.getCreateTime().getTime());
+        returnShowOutDTO.setUpdateTimestamp(aReturn.getUpdateTime().getTime());
+        return returnShowOutDTO;
     }
     @PostMapping("/updateAction")
-    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO){
-
+    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO) {
+        Return aReturn = new Return();
+        aReturn.setReturnId(returnUpdateActionInDTO.getReturnId());
+        aReturn.setAction(returnUpdateActionInDTO.getAction());
+        aReturn.setUpdateTime(new Date());
+        returnService.update(aReturn);
     }
 }
